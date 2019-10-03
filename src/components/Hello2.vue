1eef1511f10b231245f3647eaf3eecc944898940
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2019-10-04 01:05:50 +0800

    單方面傳送訊息

--- src/components/Hello2.vue
+++ src/components/Hello2.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="hello container">
+  <div class="hello container mt-5">
     <h1>{{ msg }}</h1>
     <h2>Hello2</h2>
 
+    <!-- input-group物件 -->
     <div class="input-group mb-3">
-      <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+      <input
+        type="text"
+        class="form-control"
+        placeholder="請輸入要送出的訊息"
+        aria-label="Recipient's username"
+        aria-describedby="button-addon2"
+        v-model="message_input"
+      />
       <div class="input-group-append">
-        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        <button class="btn btn-outline-secondary" @click="submitMessage()" type="button" id="button-addon2">傳送</button>
       </div>
     </div>
 
-    <div class="media my-5" v-for="item in messages">
-      <img v-if="item.username == username" src="http://lorempixel.com/100/100/sports" class="align-self-start mr-3" alt="..." />
+    <!-- media物件 -->
+    <div class="media mt-5" v-for="item in messages">
+      <img v-if="item.username == username" class="align-self-start mr-3" src="http://lorempixel.com/100/100/sports" alt="Generic placeholder image">
       <div class="media-body">
         <h5 class="mt-0">{{item.username}}</h5>
         <p>{{item.message}}</p>
       </div>
-      <img v-if="item.username != username" src="http://lorempixel.com/100/100/sports" class="align-self-start mr-3" alt="..." />
+      <img v-if="item.username != username" class="align-self-start mr-3" src="http://lorempixel.com/100/100/sports" alt="Generic placeholder image">
     </div>
-
-    <!-- <div class="media">
-      <div class="media-body">
-        <h5 class="mt-0 mb-1">Media object</h5>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </div>
-      <img src="http://lorempixel.com/100/100/sports" class="ml-3" alt="..." />
-    </div> -->
 
     <ul>
       <li>
@@ -49,19 +51,34 @@ export default {
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
-      username: 'Alice',
+      username: "Alice",
+      message_input: "",
       messages: [
         {
-          username: 'Alice',
-          message: 'Hello妳好'
+          username: "Alice",
+          message: "Hello妳好"
         },
         {
-          username: 'Bob',
-          message: 'KING of the World',
+          username: "Bob",
+          message: "KING of the World"
         }
       ]
     };
-  }
+  },
+
+  methods: {
+    submitMessage(){
+      let vm = this
+
+      // console.log(vm.message)
+      vm.messages.unshift({
+        username: vm.username,
+        message: vm.message_input
+      })
+      vm.message_input = ""
+    }
+  },
+
 };
 </script>
 
